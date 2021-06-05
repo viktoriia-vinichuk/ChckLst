8efb054eadaec21eb7d6670414c03a45c1cb9904
viv <victoria.vinichuk@gmail.com>
2021-06-05 04:32:17 +0300

    Fixed random captions issue, made code prettier

--- checklist/models.py
+++ checklist/models.py
@@ -1,162 +1,190 @@
-from django.db import models
 from django.contrib.auth.models import User
-from django.db.models import fields
+from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django_countries.fields import CountryField
 
+
 # BOOKS Models --------------------------------------------------------------------------------
 
 class Author(models.Model):
-   name = models.CharField(max_length=100)
-   def __str__(self):
-      return self.name
-   class Meta(object):
-      ordering = ['name']
+    name = models.CharField(max_length=100)
+
+    class Meta(object):
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 
 class B_Genre(models.Model):
-   genre = models.CharField(max_length=100)
-   def __str__(self):
-      return self.genre
-   class Meta(object):
-      ordering = ['genre']
+    genre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.genre
+
+    class Meta(object):
+        ordering = ['genre']
+
 
 class Book(models.Model):
 
-   order = models.PositiveIntegerField(default=0)
-   title = models.CharField(max_length=200)
-   author = models.ManyToManyField(Author)
-   genre = models.ManyToManyField(B_Genre, blank=True)
+    order = models.PositiveIntegerField(default=0)
+    title = models.CharField(max_length=200)
+    author = models.ManyToManyField(Author)
+    genre = models.ManyToManyField(B_Genre, blank=True)
 
-   def __str__(self):
-      return self.title
+    def __str__(self):
+        return self.title
 
-   class Meta(object):
-      ordering = ['order']
+    class Meta(object):
+        ordering = ['order']
 
-   def display_author(self):
-      return ', '.join(author.name for author in self.author.all())
-   display_author.short_description = 'Author'
+    def display_author(self):
+        return ', '.join(author.name for author in self.author.all())
 
-   def display_genre(self):
-      return ', '.join(genre.genre for genre in self.genre.all()[:3])
-   display_genre.short_description = 'Genre'
+    def display_genre(self):
+        return ', '.join(genre.genre for genre in self.genre.all()[:3])
+
+    display_author.short_description = 'Author'
+    display_genre.short_description = 'Genre'
+
 
 # MOVIES Models --------------------------------------------------------------------------------
 
 class M_Genre(models.Model):
-   genre = models.CharField(max_length=100)
-   def __str__(self):
-      return self.genre
-   class Meta(object):
-      ordering = ['genre']
+    genre = models.CharField(max_length=100)
+
+    class Meta(object):
+        ordering = ['genre']
+
+    def __str__(self):
+        return self.genre
+
 
 class Movie(models.Model):
 
-   order = models.PositiveIntegerField(default=0, blank=False, null=False)
-   title = models.CharField(max_length=200)
-   year = models.IntegerField()
-   genre = models.ManyToManyField(M_Genre, blank=True)
+    order = models.PositiveIntegerField(default=0, blank=False, null=False)
+    title = models.CharField(max_length=200)
+    year = models.IntegerField()
+    genre = models.ManyToManyField(M_Genre, blank=True)
 
-   def __str__(self):
-      return self.title
+    class Meta(object):
+        ordering = ['order']
 
-   class Meta(object):
-      ordering = ['order']
+    def __str__(self):
+        return self.title
 
-   def display_genre(self):
-      return ', '.join(genre.genre for genre in self.genre.all()[:3])
-   display_genre.short_description = 'Genre'
+    def display_genre(self):
+        return ', '.join(genre.genre for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
 
 # FOOD Models ----------------------------------------------------------------------------------
 
 class Ingredient(models.Model):
-   ingredient = models.CharField(max_length=100)
-   def __str__(self):
-      return self.ingredient
-   class Meta(object):
-      ordering = ['ingredient']
+    ingredient = models.CharField(max_length=100)
+
+    class Meta(object):
+        ordering = ['ingredient']
+
+    def __str__(self):
+        return self.ingredient
+
 
 class F_Kind(models.Model):
-   food_kind = models.CharField(max_length=100)
-   def __str__(self):
-      return self.food_kind
-   class Meta(object):
-      ordering = ['food_kind']
+    food_kind = models.CharField(max_length=100)
+
+    class Meta(object):
+        ordering = ['food_kind']
+
+    def __str__(self):
+        return self.food_kind
+
 
 class Food(models.Model):
 
-   order = models.PositiveIntegerField(default=0, blank=False, null=False)
-   name = models.CharField(max_length=200)
-   country = CountryField(blank=True)
-   food_kinds = models.ManyToManyField(F_Kind, blank=True)
-   food_ingredients = models.ManyToManyField(Ingredient, blank=True)
+    order = models.PositiveIntegerField(default=0, blank=False, null=False)
+    name = models.CharField(max_length=200)
+    country = CountryField(blank=True)
+    food_kinds = models.ManyToManyField(F_Kind, blank=True)
+    food_ingredients = models.ManyToManyField(Ingredient, blank=True)
 
-   def __str__(self):
-      return self.name
-    
-   class Meta(object):
-      ordering = ['order']
-      verbose_name = 'Food'
-      verbose_name_plural = 'Food'
+    class Meta(object):
+        ordering = ['order']
+        verbose_name = 'Food'
+        verbose_name_plural = 'Food'
 
-   def display_kind(self):
-      return ', '.join(food_kinds.food_kind for food_kinds in self.food_kinds.all()[:3])
-   display_kind.short_description = 'Kind'
+    def __str__(self):
+        return self.name
+
+    def display_kind(self):
+        return ', '.join(food_kinds.food_kind for food_kinds in self.food_kinds.all()[:3])
+
+    display_kind.short_description = 'Kind'
+
 
 # DRINKS Models --------------------------------------------------------------------------------
 
 class D_kind(models.Model):
-   drink_kind = models.CharField(max_length=100)
-   def __str__(self):
-      return self.drink_kind
-   class Meta(object):
-      ordering = ['drink_kind']
+    drink_kind = models.CharField(max_length=100)
+
+    class Meta(object):
+        ordering = ['drink_kind']
+
+    def __str__(self):
+        return self.drink_kind
+
 
 class Drink(models.Model):
 
-   order = models.PositiveIntegerField(default=0, blank=False, null=False)
-   name = models.CharField(max_length=200)
-   alcohol = models.BooleanField(default=False)
-   country = CountryField(blank=True)
-   drink_kinds = models.ManyToManyField(D_kind, blank=True)
-   drink_ingredients = models.ManyToManyField(Ingredient, blank=True)
+    order = models.PositiveIntegerField(default=0, blank=False, null=False)
+    name = models.CharField(max_length=200)
+    alcohol = models.BooleanField(default=False)
+    country = CountryField(blank=True)
+    drink_kinds = models.ManyToManyField(D_kind, blank=True)
+    drink_ingredients = models.ManyToManyField(Ingredient, blank=True)
 
-   def __str__(self):
-      return self.name
+    class Meta(object):
+        ordering = ['order']
 
-   class Meta(object):
-      ordering = ['order']
+    def __str__(self):
+        return self.name
 
-   def display_kind(self):
-      return ', '.join(drink_kinds.drink_kind for drink_kinds in self.drink_kinds.all()[:3])
-   display_kind.short_description = 'Type'
+    def display_kind(self):
+        return ', '.join(drink_kinds.drink_kind for drink_kinds in self.drink_kinds.all()[:3])
+
+    display_kind.short_description = 'Type'
+
 
 # User Profile Models -------------------------------------------------------------------------
 
 class Choice(models.Model):
-   user = models.OneToOneField(User, on_delete=models.CASCADE)
-   books = models.ManyToManyField(Book, blank=True)
-   movies = models.ManyToManyField(Movie, blank=True)
-   food = models.ManyToManyField(Food, blank=True)
-   drinks = models.ManyToManyField(Drink, blank=True)
-   
-   def __str__(self):
-      return 'User_choice'
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    books = models.ManyToManyField(Book, blank=True)
+    movies = models.ManyToManyField(Movie, blank=True)
+    food = models.ManyToManyField(Food, blank=True)
+    drinks = models.ManyToManyField(Drink, blank=True)
+
+    def __str__(self):
+        return 'User_choice'
+
 
 @receiver(post_save, sender=User)
 def create_choice(sender, instance, created, **kwargs):
-   if created:
-      Choice.objects.create(user=instance)
+    if created:
+        Choice.objects.create(user=instance)
+
 
 # Stats Model ---------------------------------------------------------------------------------
 
 class Statistic(models.Model):
-   user_id = models.OneToOneField(User, on_delete=models.CASCADE)
-   num_books = models.PositiveIntegerField(default=0, blank=False, null=False)
-   num_movies = models.PositiveIntegerField(default=0, blank=False, null=False)
-   num_food = models.PositiveIntegerField(default=0, blank=False, null=False)
-   num_drinks = models.PositiveIntegerField(default=0, blank=False, null=False)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    num_books = models.PositiveIntegerField(default=0, blank=False, null=False)
+    num_movies = models.PositiveIntegerField(default=0, blank=False, null=False)
+    num_food = models.PositiveIntegerField(default=0, blank=False, null=False)
+    num_drinks = models.PositiveIntegerField(default=0, blank=False, null=False)
 
-   def __str__(self):
-      return str(self.user_id.username)
+    def __str__(self):
+        return str(self.user_id.username)
--- checklist/templatetags/tags.py
+++ checklist/templatetags/tags.py
@@ -1,23 +1,29 @@
 from django import template
+
 from .. import views
 
 register = template.Library()
 
+
 @register.simple_tag
-def is_marked(object,user):
+def is_marked(object, user):
     return user in [item.user for item in object.choice_set.all()]
+
 
 @register.simple_tag
 def is_all_read(user):
     return views.is_all_read(user)
 
+
 @register.simple_tag
 def is_all_watched(user):
     return views.is_all_watched(user)
 
+
 @register.simple_tag
 def is_all_eaten(user):
     return views.is_all_eaten(user)
+
 
 @register.simple_tag
 def is_all_drunk(user):
